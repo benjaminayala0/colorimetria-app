9c5ab84f48d95880045a3bea4af974086d27efcf
unknown <benjaayala140@gmail.com>
2026-01-07 20:53:38 -0300

    fix: switch database from SQLite to PostgreSQL to ensure data persistence

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,10 +1,16 @@
+require ('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 // Initialize Sequelize with SQLite
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite', 
-    logging: false // 
+ const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres', 
+    logging: false ,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    } 
 });
 
 module.exports = sequelize;
