36780da348db3dd3eae56d95f6f667accac88feb
unknown <benjaayala140@gmail.com>
2026-02-05 14:26:18 -0300

    feat: config hibrida local-produccion db

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,15 +1,26 @@
 require ('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+const dbUrl = process.env.DATABASE_URL ;
+
+if (!dbUrl) {
+    console.error('DATABASE_URL is not defined in the environment variables.');
+}
+
+const isLocal = dbUrl && (dbUrl.includes('localhost') || dbUrl.includes('127.0.0.1'));
+
 // Initialize Sequelize with SQLite
  const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres', 
     logging: false ,
     dialectOptions: {
-        ssl: {
+        ssl: isLocal ? false : {
             require: true,
             rejectUnauthorized: false
-        }
+        }, 
+        
+        family: 4
+
     } 
 });
 
