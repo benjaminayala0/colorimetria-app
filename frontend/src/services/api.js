00ef78f33400750cd3781443793b8186bf0698be
unknown <benjaayala140@gmail.com>
2026-02-18 17:28:59 -0300

    update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-const BASE_URL = 'http://192.168.1.6:3000';
+const BASE_URL = 'https://colorimetria-app.onrender.com';
 
 const api = axios.create({
     baseURL: BASE_URL,
